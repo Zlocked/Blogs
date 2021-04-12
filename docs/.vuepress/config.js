219426a449010a4742323876ffd03101521c944b
Zlocked <515899406@qq.com>
2021-04-12 09:25:54 +0800

    update: config fixed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,10 +27,10 @@ module.exports = {
 		nav: nav,
 		// 博客配置
 		blogConfig: {
-			category: {
-				location: 2, // 在导航栏菜单中所占的位置，默认2
-				text: 'Blogs' // 默认文案 “分类”
-			},
+			// category: {
+			// 	location: 2, // 在导航栏菜单中所占的位置，默认2
+			// 	text: 'Blogs' // 默认文案 “分类”
+			// },
 			tag: {
 				location: 3, // 在导航栏菜单中所占的位置，默认3
 				text: 'Tag' // 默认文案 “标签”
